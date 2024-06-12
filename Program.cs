837ac59e1dc67dad27627b5195756a823cdc0963
IP-13Vadim <90095400+krvvadim@users.noreply.github.com>
2024-06-12 22:25:01 +0300

    add edit files

--- Program.cs
+++ Program.cs
@@ -10,40 +10,51 @@ namespace SelectionSort
         {
             var cars = CarFactory.GenerateCars();
 
-            // Sequential sort
+            //Sequential sort
             var carsSequential = new List<Car>(cars);
-            Console.WriteLine("List of cars:");
-            PrintCars(carsSequential);
+            //Console.WriteLine("List of cars:");
+            //PrintCars(carsSequential);
             DateTime time1 = DateTime.Now;
             Sorter.SelectionSort(carsSequential);
             DateTime time2 = DateTime.Now;
 
-            Console.WriteLine("\nTime for sequential sorting: " + time2.Subtract(time1).TotalSeconds + " seconds");
+            Console.WriteLine("\nTime for sequential sorting: " + time2.Subtract(time1).TotalSeconds * 1000000 + " microseconds");
 
-            // Parallel sort
-            var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 8 }; // Example: using n processors
-            var carsParallel = new List<Car>(cars);
-            DateTime time3 = DateTime.Now;
-            ParallelSorter.ParallelSelectionSort(carsParallel, parallelOptions);
-            DateTime time4 = DateTime.Now;
+            //Console.WriteLine("\nSequential Sort:");
+            //PrintCars(carsSequential);
 
-            Console.WriteLine("\nTime for parallel sorting: " + time4.Subtract(time3).TotalSeconds + " seconds");
+            int[] array = new int[3] { 2, 4, 8 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                // Parallel sort
+                var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = array[i] }; // Example: using n processors
+                var carsParallel = new List<Car>(cars);
+                //Console.WriteLine("\nList of cars:");
+                //PrintCars(carsParallel);
+                DateTime time3 = DateTime.Now;
+                ParallelSorter.ParallelSelectionSort(carsParallel, parallelOptions);
+                DateTime time4 = DateTime.Now;
 
-            Console.WriteLine("\nSequential Sort:");
-            PrintCars(carsSequential);
+                Console.WriteLine("\nTime for parallel sorting " + array[i] + ": " + time4.Subtract(time3).TotalSeconds * 1000000 + " microseconds");
 
-            Console.WriteLine("\nParallel Sort:");
-            PrintCars(carsParallel);
+                //Console.WriteLine("\nParallel Sort:");
+                //PrintCars(carsParallel);
+            }
         }
 
         private static void PrintCars(List<Car> cars)
         {
-            const int maxToPrint = 30; // Number of elements to print
-            for (int i = 0; i < cars.Count && i < maxToPrint; i++)
+            const int maxToPrint = 15; // Number of elements to print
+            Console.WriteLine("First 15 cars:");
+            for (int i = 0; i < maxToPrint; i++)
+            {
+                Console.WriteLine($"Car {cars[i].Number}: Length = {cars[i].Length:F2}");
+            }
+            Console.WriteLine("\nLast 15 cars:");
+            for (int i = cars.Count - maxToPrint; i < cars.Count; i++)
             {
                 Console.WriteLine($"Car {cars[i].Number}: Length = {cars[i].Length:F2}");
             }
         }
-
     }
 }
